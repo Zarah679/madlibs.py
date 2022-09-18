b9778b02a89a6a5312412330a0fd983d8c5333ac
Fatima S <84390176+Zarah679@users.noreply.github.com>
2022-09-18 21:35:40 +0100

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+#madlibs
+#some string variables
+
+adj1 = input("Adjective: ") # input promting the user for a response
+adj2 = input ("Another Adjective: ")
+adj3 = input ("Some other Adjective: ")
+verb = input ("Verb: ")
+noun = input ("noun: ")
+famous_person = input ("famous_person: ")
+
+madlib = f"Computer programming is {adj1}! Although it can be {adj2} sometimes, it makes me so {adj3} because i love {verb} and always get some {noun} like we're {famous_person}!"
+
+print(madlib)
